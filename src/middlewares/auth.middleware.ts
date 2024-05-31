b7c68bf3889db2bca01f857007b99490ce1223ae
SebastianHinestroza12 <menas7527@gmail.com>
2024-05-31 09:57:48 -0500

    feat(middlewares): authentication middleware

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,35 @@
+import { verifyToken } from '../utils/jwt';
+import { Request, Response, NextFunction } from 'express';
+import { JwtPayload } from 'jsonwebtoken';
+
+interface CustomRequest extends Request {
+  user?: string | JwtPayload;
+}
+
+const authMiddleware = (req: CustomRequest, res: Response, next: NextFunction): Response | void => {
+  const excludedPaths = [
+    '/api/v1/auth/login',
+    '/api/v1/auth/register',
+    '/api/v1/auth/recover-password',
+  ];
+
+  if (excludedPaths.includes(req.path)) {
+    return next();
+  }
+
+  const { token } = req.cookies;
+
+  if (!token || typeof token !== 'string') {
+    return res.status(401).json({ message: 'Access denied. No token provided.' });
+  }
+
+  try {
+    const decoded = verifyToken(token);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Unauthorized access' });
+  }
+};
+
+export { authMiddleware };
