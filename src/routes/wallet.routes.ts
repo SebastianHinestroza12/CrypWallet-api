6e8cdd90f50bcd466107a0c07948627bee571ead
SebastianHinestroza12 <menas7527@gmail.com>
2024-06-10 22:57:08 -0500

    Refactor(Services/wallet): Services delete and find wallet for id

--- src/routes/wallet.routes.ts
+++ src/routes/wallet.routes.ts
@@ -5,7 +5,9 @@ import { WalletController } from '../controllers/wallet.controller';
 const walletRoutes = Router();
 
 walletRoutes.post('/:id', WalletController.createWallet);
-walletRoutes.get('/:id', WalletController.getWalletByUserId);
+walletRoutes.get('/user/:id', WalletController.getWalletByUserId);
+// walletRoutes.get('/:wallet_id', WalletController.getWalletByUserId);
+// walletRoutes.delete('/:wallet_id', WalletController.getWalletByUserId);
 walletRoutes.patch('/update/:id', WalletController.setWalletById);
 
 export { walletRoutes };
