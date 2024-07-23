046f2c81cdedae26821fe94f3351c5bd4e67f3e4
SebastianHinestroza12 <menas7527@gmail.com>
2024-07-23 17:38:05 -0500

    refactor: Refactoring in payments, to use and create multiple providers

--- src/interfaces/payment-provider.ts
+++ src/interfaces/payment-provider.ts
@@ -1,8 +1,27 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { SessionData } from './session-data-stripe';
-import { StripeResponse } from './stripe-response';
+export interface PaymentProvider<T> {
+  createPayment(sessionData: any): Promise<T>;
+}
 
-export interface PaymentProvider {
-  createPayment(sessionData: SessionData): Promise<StripeResponse>;
+export interface PreferenceData {
+  body: {
+    items: {
+      id: string;
+      title: string;
+      picture_url: string;
+      quantity: number;
+      currency_id: string;
+      unit_price: number;
+    }[];
+    payer: {
+      email: string;
+    };
+    back_urls: {
+      success: string;
+      failure: string;
+      pending: string;
+    };
+    auto_return: string;
+  };
 }
